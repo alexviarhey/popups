18715ca239bc06d2a7b8fc8119c9d4de71d4da7c
alexviarhey <lexa.vergey@gmail.com>
2019-11-10 14:56:48 +0300

    First

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Popup from "./components/Popup";
+import {connect} from 'react-redux';
+import {IPopup} from "./types/popup-types";
+import {AppStateType} from './redux/store';
+import {addPopup} from './redux/popup-reducer';
+import Buttons from "./components/Buttons";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+interface IMapStateProps {
+    popups: Array<IPopup>,
+    id: number
 }
 
-export default App;
+interface IMapDispatchProps {
+    addPopup: (popup: IPopup, id: number) => void
+}
+
+type CommonProps = IMapStateProps & IMapDispatchProps
+
+const App = (props: CommonProps) => {
+
+    const addCommonPopup = (title: string, type: string) => {
+        const newPopup: IPopup = {
+            id: props.id,
+            title,
+            type
+        };
+        props.addPopup(newPopup, props.id)
+    }
+
+    return (
+        <div className='app'>
+            <Buttons addCommonPopup={addCommonPopup}/>
+            <div className='popups'>
+                {props.popups.length > 0 && props.popups.map(p => <Popup key={p.id} type={p.type}>{p.title}</Popup>)}
+            </div>
+        </div>
+    )
+};
+
+
+const mapStateToProps = (state: AppStateType): IMapStateProps => {
+    return {
+        id: state.popups.id,
+        popups: state.popups.popups
+    }
+};
+
+const mapDispatchToProps = (dispatch: any): IMapDispatchProps => {
+    return {
+        addPopup: (popup, id) => {
+            dispatch(addPopup(popup, id))
+        }
+    }
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
